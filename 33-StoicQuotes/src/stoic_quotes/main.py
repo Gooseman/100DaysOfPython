4760b66b5d69f70b1fd09b3cf08f2755035a0998
Craig Barrett <craigtheb@duck.com>
2026-09-18 17:33:17 +0100

    Stoic Quotes update
    Update the Stoic Quotes project to retrieve a quote and send it on
    telegram rather than showing it in a UI.
    
    Update of the main Github action execution of the Stoic Quotes project
    to use the telegram secrets.

--- 33-StoicQuotes/src/stoic_quotes/main.py
+++ 33-StoicQuotes/src/stoic_quotes/main.py
@@ -1,14 +1,16 @@
 from stoic_quotes.quote_retriever import QuoteRetriever
 from stoic_quotes.quote_ui import QuoteUi
+from stoic_quotes.telegram import send_telegram_message
 
-def get_quote():
+def send_quote():
     quote, author = QuoteRetriever().get_quote()
 
     if quote and author:
         print(f'"{quote}" - {author}')
+        send_telegram_message(f'"{quote}" - {author}')
     else:
         print("Failed to retrieve quote.")
 
 if __name__ == "__main__":
-    # get_quote()
-    QuoteUi(QuoteRetriever().get_quote)
+    # QuoteUi(QuoteRetriever().get_quote)
+    send_quote()
